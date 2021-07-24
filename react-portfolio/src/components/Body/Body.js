3ff8650d629ac4e179db148a606be32b01c3d9be
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-24 16:20:14 -0400

    Created Footer.js and Body.js files and corresponding css in the components folder.

---
+++ react-portfolio/src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from './Header';
+
+function Body(props) {
+    console.log(props);
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
--- react-portfolio/src/components/Header/Header.js
+++ react-portfolio/src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Header.css';
 
-function body() {
+function body(props) {
     return (
         <div className="body">
             <div className="container">
                 <div className="header">
-                    <h1>Ashleigh Freiler</h1>
-                    <p>An evolving human seeking knowledge in all things great and small.</p>
+                    <h1 className="name">Ashleigh Freiler</h1>
+                    <p className="tag">An evolving human seeking knowledge in all things great and small.</p>
                 </div>
 
             </div>
