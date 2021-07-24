7bb16754b8a027ae2a9291bd2e2fa49738a8b806
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-23 21:49:20 -0400

    Created the Navbar folder.

--- react-portfolio/src/components/Header/Header.js
+++ react-portfolio/src/components/Header/Header.js
@@ -5,6 +5,10 @@ function body() {
     return (
         <div className="body">
             <div className="container">
+                <div className="header">
+                    <h1>Ashleigh Freiler</h1>
+                    <p>An evolving human seeking knowledge in all things great and small.</p>
+                </div>
 
             </div>
         </div>
