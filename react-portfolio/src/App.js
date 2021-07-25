b1908f2bfe0cbdfe9bbfb4fc8cd8baf5f718655f
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-25 10:50:08 -0400

    Added the Recipe Box project to the Portfolio.js file

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import AboutMe from './components/AboutMe/AboutMe';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Portfolio from './components/Portfolio/Portfolio';
 // import Navbar from './components/Navbar';
 // import Body from './components/Body';
 // import Footer from './components/Footer'
@@ -13,7 +14,8 @@ function App() {
   return (
     <div>
       <Header />
-      <AboutMe />
+      <Portfolio />
+      {/* <AboutMe /> */}
       <Footer />
     </div>
   );
---
+++ react-portfolio/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div className="portfolio">
+            <div className="container">
+                <h1>Portfolio</h1>
+                <div className="project">
+                    <h2>Title</h2>
+                    <p>Description</p>
+                    <img src="" alt="" />
+                    <a href="">Placeholder</a>
+                </div>
+                <div className="project">
+                    <h2>Title</h2>
+                    <p>Description</p>
+                    <img src="" alt="" />
+                    <a href="">Placeholder</a>
+                </div>
+                <div className="project">
+                    <h2>Recipe Box</h2>
+                    <p>Recipe Box is an application that allows a user to input a desired meal choice with the expected result of a recipe for said meal. The recipe will be accompanied by an image that corresponds to the meal choice.</p>
+                    <img src="RB.jpg" alt="Recipe Box Homepage" />
+                    <a href="https://arfreiler.github.io/Recipe-Box/">Recipe Box</a>
+                </div>
+                <div className="project">
+                    <h2>Title</h2>
+                    <p>Description</p>
+                    <img src="" alt="" />
+                    <a href="">Placeholder</a>
+                </div>
+                <div className="project">
+                    <h2>Title</h2>
+                    <p>Description</p>
+                    <img src="" alt="" />
+                    <a href="">Placeholder</a>
+                </div>
+                <div className="project">
+                    <h2>Title</h2>
+                    <p>Description</p>
+                    <img src="" alt="" />
+                    <a href="">Placeholder</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Portfolio;
