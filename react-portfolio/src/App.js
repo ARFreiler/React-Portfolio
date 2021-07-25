8781e123e086a4001875f4f121f6dd6e977c9978
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-25 18:05:05 -0400

    Created Contact folder and corresponding Contact.js and Contact.css files and set imports.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
 import './App.css';
-import AboutMe from './components/AboutMe/AboutMe';
+// import AboutMe from './components/AboutMe/AboutMe';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Portfolio from './components/Portfolio/Portfolio';
+// import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
 // import Navbar from './components/Navbar';
-// import Body from './components/Body';
-// import Footer from './components/Footer'
+
 
 function App() {
 
   return (
     <div>
       <Header />
-      <Portfolio />
       {/* <AboutMe /> */}
+      {/* <Portfolio /> */}
+      <Contact />
       <Footer />
     </div>
   );
---
+++ react-portfolio/src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div className="container">
+                <h2>Contact Me</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
