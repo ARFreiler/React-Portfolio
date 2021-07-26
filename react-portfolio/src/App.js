f13f566f379b84a07beb584c4f189e0a5ee8497d
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-25 21:31:48 -0400

    Created the CV folder with its corresponding CV.js and CV.css files.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 // import AboutMe from './components/AboutMe/AboutMe';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-// import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
+import Portfolio from './components/Portfolio/Portfolio';
+// import Contact from './components/Contact/Contact';
 // import Navbar from './components/Navbar';
 
 
@@ -15,8 +15,8 @@ function App() {
     <div>
       <Header />
       {/* <AboutMe /> */}
-      {/* <Portfolio /> */}
-      <Contact />
+      <Portfolio />
+      {/* <Contact /> */}
       <Footer />
     </div>
   );
---
+++ react-portfolio/src/components/CV/CV.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './CV.css';
+
+const CV = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default CV;
