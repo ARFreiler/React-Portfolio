30dca2a6ee1c34874cbb3d8f002c6bfea53803e3
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-07-25 22:01:16 -0400

    Installed fontawesome react npm and added twitter icon to footer.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 // import AboutMe from './components/AboutMe/AboutMe';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Portfolio from './components/Portfolio/Portfolio';
+// import Portfolio from './components/Portfolio/Portfolio';
 // import Contact from './components/Contact/Contact';
 // import Navbar from './components/Navbar';
 
@@ -15,7 +15,7 @@ function App() {
     <div>
       <Header />
       {/* <AboutMe /> */}
-      <Portfolio />
+      {/* <Portfolio /> */}
       {/* <Contact /> */}
       <Footer />
     </div>
--- react-portfolio/src/components/Footer/Footer.js
+++ react-portfolio/src/components/Footer/Footer.js
@@ -1,17 +1,46 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FaTwitter } from "react-icons/fa"
+
 import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="center-text">
-                <p className="copyright">
-                    RoseAshTech © 2021 LLC. All Rights Reserved
-                </p>
-            </div>
-        </footer>
-    );
-};
+        <div>
+            <FaTwitter />
+        </div>
+    )
+}
+
+// const element = <FontAwesomeIcon icon={faCoffee} />
+
+// ReactDOM.render(element, document.body)
+
+// const Footer = () => {
+//     return (
+//         <footer className="footer">
+//             <div class="container">
+//                 <div class="icons">
+//                     <a class="icons" href="https://github.com/ARFreiler"
+//                     ><i class="fab fa-github-square fa-3x"></i
+//                     ></a>
+//                     <a class="icons" href="https://linkedin.com/in/ashleighfreiler"
+//                     ><i class="fab fa-linkedin fa-3x"></i
+//                     ></a>
+//                     <a class="icons" href="https://twitter.com/AshleighFreiler"
+//                     ><i class="fab fa-twitter fa-3x"></i
+//                     ></a>
+//                 </div>
+//             </div>
+//             <div className="center-text">
+//                 <p className="copyright">
+//                     RoseAshTech © 2021 LLC. All Rights Reserved
+//                 </p>
+//             </div>
+//         </footer>
+//     );
+// };
 
 export default Footer;
